e294dee7c4c7f6ec50f9fa614ce7e1a3fef6367d
henrique ferraz <henriqueferraz@gmail.com>
2024-02-17 19:39:12 -0300

    1702

--- src/controllers/peoples.ts
+++ src/controllers/peoples.ts
@@ -30,3 +30,69 @@ export const getPerson: RequestHandler = async (req, res) => {
 
     res.json({ error: 'Ocorreu um erro aqui - PEOPLE002' })
 }
+
+//TESTADO
+export const addPerson: RequestHandler = async (req, res) => {
+    const { id_event, id_group } = req.params
+
+    const addPersonSchema = z.object({
+        name: z.string(),
+        cpf: z.string().transform(val => val.replace(/\.|-|,/gm, ''))
+    })
+
+    const body = addPersonSchema.safeParse(req.body)
+    if (!body.success) return res.json({ error: 'Dados Inválidos - PEOPLE003' })
+
+    const newPerson = await people.add({
+        name: body.data.name,
+        cpf: body.data.cpf,
+        id_event: parseInt(id_event),
+        id_group: parseInt(id_group)
+    })
+
+    if (newPerson) return res.status(201).json({ person: newPerson })
+
+    res.json({ error: 'Ocorreu um erro aqui - PEOPLE004' })
+}
+
+//TESTADO
+export const updatePerson: RequestHandler = async (req, res) => {
+    const { id, id_event, id_group } = req.params
+
+    const updatePersonSchema = z.object({
+        name: z.string().optional(),
+        cpf: z.string().transform(val => val.replace(/\.|-|,/gm, '')).optional(),
+        matched: z.string().optional()
+    })
+    const body = updatePersonSchema.safeParse(req.body)
+    if (!body.success) return res.json({ error: 'Dados Inválidos - PEOPLE005' })
+
+    const updatedPerson = await people.update({
+        id: parseInt(id),
+        id_event: parseInt(id_event),
+        id_group: parseInt(id_group)
+    }, body.data)
+
+    if (updatedPerson) {
+        const personItem = await people.getOne({
+            id: parseInt(id),
+            id_event: parseInt(id_event)
+        })
+        return res.json({ person: personItem })
+    }
+
+    res.json({ error: 'Ocorreu um erro aqui - PEOPLE006' })
+}
+
+export const deletePerson: RequestHandler = async (req, res) => {
+    const { id, id_event, id_group } = req.params
+    const deletedPerson = await people.remove({
+        id: parseInt(id),
+        id_event: parseInt(id_event),
+        id_group: parseInt(id_group)
+    })
+
+    if (deletedPerson) return res.json({ person: deletedPerson })
+
+    res.json({ error: 'Ocorreu um erro aqui - PEOPLE007' })
+}
--- src/router/admin.ts
+++ src/router/admin.ts
@@ -27,9 +27,12 @@ router.post('/event/:id_event/groups/', auth.validate, groups.addGroup)//TESTADO
 router.put('/event/:id_event/groups/:id', auth.validate, groups.updateGroup)//TESTADO
 router.delete('/event/:id_event/groups/:id', auth.validate, groups.deleteGroup)//TESTADO
 
+//rotas de grupos testado e funcionando - 17/02
 router.get('/event/:id_event/groups/:id_group/people', auth.validate, people.getAll)//TESTADO
 router.get('/event/:id_event/groups/:id_group/people/:id', auth.validate, people.getPerson)//TESTADO
-//router.post('/event/:id_event/groups/:id_group/people', auth.validate, people.addPerson)
+router.post('/event/:id_event/groups/:id_group/people', auth.validate, people.addPerson)//TESTADO
+router.put('/event/:id_event/groups/:id_group/people/:id', auth.validate, people.updatePerson)//TESTADO
+router.delete('/event/:id_event/groups/:id_group/people/:id', auth.validate, people.deletePerson)//TESTADO
 
 
 export default router;
